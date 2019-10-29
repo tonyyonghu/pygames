8256851304c7e019ed9fbd7f46a913768ceba42e
tonyyonghu <1154771388@qq.com>
2019-10-29 16:08:41 +0800

    将代码改成函数

--- 鼠标画多边形.py
+++ 鼠标画多边形.py
@@ -1,10 +1,10 @@
 import pygame
 from pygame.locals import *
+import sys
+width=980         #窗口宽
+height = 600      #窗口高
 
-width=980
-height = 600
-
-WHITE=(255,255,255)
+WHITE=(255,255,255)  #颜色
 
 BLUE=(0,0,255)
 
@@ -14,38 +14,46 @@ RED=(255,0,0)
 
 BLACK=(0,0,0)
 
-pygame.init()
 
-screen=pygame.display.set_mode((width,height))
 
 background=GREEN
 
-running=True
-drawing = False
-points=[]
 
-while running:
-	for event in pygame.event.get():
+  #储存点的变量
 
-		if event.type==QUIT:
-			running=False
-		elif event.type==KEYDOWN:
-			if event.key==K_ESCAPE:
-				print(event.key)
-				if len(points)>0:
-					points.pop()
-		elif event.type==MOUSEBUTTONDOWN:
-			points.append(event.pos)
-			drawing=True
-		elif event.type==MOUSEBUTTONUP:
-			drawing=False
+def draw_triggle():
+	running=True
+	drawing = False
+	points=[]
+	pygame.init()    #初始化
+	screen=pygame.display.set_mode((width,height))   #设置屏幕对象
+	while running:
+		for event in pygame.event.get():  #循环所有的事件
 
-		elif event.type==pygame.MOUSEMOTION and drawing:
-			points[-1]=event.pos
+			if event.type==QUIT:
+				running=False
+			elif event.type==KEYDOWN:      #键盘按钮按下事件
 
-	screen.fill(background)
-	if len(points)>1:
-		rect=pygame.draw.lines(screen,RED,True,points,5)
-		pygame.draw.rect(screen,GREEN,rect,1)
-	pygame.display.update()
-pygame.quit()
+				if event.key==K_ESCAPE:    #按esc键
+					
+					if len(points)>0:      
+						points.pop()       #抛出列表中最后一个元素
+			elif event.type==MOUSEBUTTONDOWN:     #鼠标按下事件
+				print(event.pos)
+				points.append(event.pos)
+				drawing=True
+			elif event.type==MOUSEBUTTONUP:       #鼠标松开事件
+				drawing=False
+
+			elif event.type==pygame.MOUSEMOTION and drawing:
+				points[-1]=event.pos
+
+		screen.fill(background)
+		if len(points)>1:
+			rect=pygame.draw.lines(screen,RED,True,points,5)
+			pygame.draw.rect(screen,GREEN,rect,1)
+		pygame.display.update()
+	pygame.quit()
+
+
+draw_triggle()
